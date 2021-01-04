eb1a2038421e98c2e8adcfc5a2bd42129fa0a037
julien <julien.kennel@geauto.fr>
2021-01-04 11:58:44 +0100

    ajout photo couverture + zinguerie

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -28,7 +28,22 @@ class FrontController extends Controller
 			'img/zinguerie/zinguerie_5.jpg',
 			'img/zinguerie/zinguerie_6.jpg',
 			'img/zinguerie/zinguerie_7.jpg',
-			'img/zinguerie/zinguerie_1.jpg'
+			'img/zinguerie/zinguerie_1.jpg',
+            'img/zinguerie/chantier_jung_1.jpg',
+            'img/zinguerie/chantier_jung_2.jpg',
+            'img/zinguerie/chantier_jung_3.jpg',
+            'img/zinguerie/chantier_jung_4.jpg',
+            'img/zinguerie/cuivre_1.jpg',
+            'img/zinguerie/cuivre_2.jpg',
+            'img/zinguerie/cuivre_3.jpg',
+            'img/zinguerie/chantier_planche_de_rive_1.jpg',
+            'img/zinguerie/decrochement_gouttieres.jpg',
+            'img/zinguerie/gouttieres.jpg',
+            'img/zinguerie/gouttieres_hoerdt.jpg',
+            'img/zinguerie/habillage_de_cheminee.jpg',
+            'img/zinguerie/tuyau_de_descente.jpg',
+            'img/zinguerie/tuyau_descente.jpg',
+
     	];
 
     	for ($i=0; $i < count($photos); $i++) { 
@@ -49,6 +64,17 @@ class FrontController extends Controller
 			'img/couverture/couverture_4.jpg',
 			'img/couverture/couverture_5.jpg',
 			'img/couverture/couverture_6.JPG',
+            'img/couverture/20.jpg',
+            'img/couverture/21.jpg',
+            'img/couverture/charpente_weyersheim.jpg',
+            'img/couverture/rive_apres.jpg',
+            'img/couverture/rive_de_proche.jpg',
+            'img/couverture/toit_fini_2.jpg',
+            'img/couverture/toit_pendant.jpg',
+            'img/couverture/toiture_hoerdt.jpg',
+            'img/couverture/toiture_hoerdt_2.jpg',
+            'img/couverture/toiture_hoerdt_arriere.jpg',
+            'img/couverture/tuile_tradi_12.jpg',
     	];
 
     	for ($i=0; $i < count($photos); $i++) { 
